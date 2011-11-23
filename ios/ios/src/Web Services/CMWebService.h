d74e46707a3a02081923b10d15a539e538851bf7
Marc Weil <marc@cloudmine.me>
2011-11-22 22:05:56 -0500

    Lots of docblocks.

--- ios/ios/src/Web Services/CMWebService.h
+++ ios/ios/src/Web Services/CMWebService.h
@@ -50,7 +50,7 @@
  * Asynchronously retrieve objects for the named app-level keys. On completion, the <tt>successHandler</tt> block 
  * will be called with a dictionary of the objects retrieved as well as a dictionary of the key-related errors returned from the server.
  *
- * @param keys The keys to fetch.
+ * @param keys The keys to fetch. If <tt>nil</tt> or an empty array, all objects will be returned.
  * @param successHandler The block to be called when the objects have been populated.
  * @param errorHandler The block to be called if the entire request failed (i.e. if there is no network connectivity).
  */
@@ -69,24 +69,89 @@
 - (void)getValuesForKeys:(NSArray *)keys withUserCredentials:(CMUserCredentials *)credentials
           successHandler:(void (^)(NSDictionary *results, NSDictionary *errors))successHandler errorHandler:(void (^)(NSError *error))errorHandler;
 
+/**
+ * Asynchronously update one or more objects for the app-level keys included in <tt>data</tt>. On completion, the <tt>successHandler</tt>  
+ * block will be called with a dictionary of the keys of the objects that were created and updated as well as a dictionary of the
+ * key-related errors returned from the server.
+ *
+ * @param data A dictionary mapping top-level keys to the values to be used to update the object.
+ * @param successHandler The block to be called when the objects have been populated.
+ * @param errorHandler The block to be called if the entire request failed (i.e. if there is no network connectivity).
+ */
 - (void)updateValuesFromDictionary:(NSDictionary *)data successHandler:(void (^)(NSDictionary *results, NSDictionary *errors))successHandler 
                errorHandler:(void (^)(NSError *error))errorHandler;
 
+/**
+ * Asynchronously update one or more objects for the user-level keys included in <tt>data</tt>. On completion, the <tt>successHandler</tt>  
+ * block will be called with a dictionary of the keys of the objects that were created and updated as well as a dictionary of the
+ * key-related errors returned from the server.
+ *
+ * @param data A dictionary mapping top-level keys to the values to be used to update the object.
+ * @param credentials The user identifier and password of the user.
+ * @param successHandler The block to be called when the objects have been populated.
+ * @param errorHandler The block to be called if the entire request failed (i.e. if there is no network connectivity).
+ */
 - (void)updateValuesFromDictionary:(NSDictionary *)data withUserCredentials:(CMUserCredentials *)credentials 
              successHandler:(void (^)(NSDictionary *results, NSDictionary *errors))successHandler errorHandler:(void (^)(NSError *error))errorHandler;
 
+/**
+ * Asynchronously create or replace one or more objects for the values of the app-level keys included in <tt>data</tt>. On completion, the <tt>successHandler</tt>  
+ * block will be called with a dictionary of the keys of the objects that were created and replaced as well as a dictionary of the
+ * key-related errors returned from the server.
+ *
+ * Note that if the key already exists server-side, this method will fully replace its value. For updating via merge, see the <tt>updateValuesFromDictionary</tt> methods.
+ *
+ * @see updateValuesFromDictionary:successHandler:errorHandler:
+ *
+ * @param data A dictionary mapping top-level keys to the values to be used to update the object.
+ * @param successHandler The block to be called when the objects have been populated.
+ * @param errorHandler The block to be called if the entire request failed (i.e. if there is no network connectivity).
+ */
 - (void)setValuesFromDictionary:(NSDictionary *)data successHandler:(void (^)(NSDictionary *results, NSDictionary *errors))successHandler 
             errorHandler:(void (^)(NSError *error))errorHandler;
 
+/**
+ * Asynchronously create or replace one or more objects for the values of the user-level keys included in <tt>data</tt>. On completion, the <tt>successHandler</tt>  
+ * block will be called with a dictionary of the keys of the objects that were created and replaced as well as a dictionary of the
+ * key-related errors returned from the server.
+ *
+ * Note that if the key already exists server-side, this method will fully replace its value. For updating via merge, see the <tt>updateValuesFromDictionary</tt> methods.
+ *
+ * @see updateValuesFromDictionary:userCredentials:successHandler:errorHandler:
+ *
+ * @param data A dictionary mapping top-level keys to the values to be used to update the object.
+ * @param successHandler The block to be called when the objects have been populated.
+ * @param errorHandler The block to be called if the entire request failed (i.e. if there is no network connectivity).
+ */
 - (void)setValuesFromDictionary:(NSDictionary *)data withUserCredentials:(CMUserCredentials *)credentials 
           successHandler:(void (^)(NSDictionary *results, NSDictionary *errors))successHandler errorHandler:(void (^)(NSError *error))errorHandler;
 
-- (void)deleteAllWithSuccessHandler:(void (^)(void)) errorHandler:(void (^)(NSError *error))errorHandler;
-
+/**
+ * Asynchronously delete objects for the named app-level keys. On completion, the <tt>successHandler</tt> block 
+ * will be called.
+ *
+ * For the sake of consistency, <tt>results</tt> and <tt>errors</tt> will be sent to the callback like with all the other methods in this class,
+ * however they will <strong>always</strong> be empty.
+ *
+ * @param keys The keys to delete. If <tt>nil</tt> or an empty array, <strong>all objects will be deleted.</strong>
+ * @param successHandler The block to be called when the objects have been populated.
+ * @param errorHandler The block to be called if the entire request failed (i.e. if there is no network connectivity).
+ */
 - (void)deleteValuesForKeys:(NSArray *)keys successHandler:(void (^)(NSDictionary *results, NSDictionary *errors))successHandler 
                errorHandler:(void (^)(NSError *error))errorHandler;
 
+/**
+ * Asynchronously delete objects for the named user-level keys. On completion, the <tt>successHandler</tt> block 
+ * will be called.
+ *
+ * For the sake of consistency, <tt>results</tt> and <tt>errors</tt> will be sent to the callback like with all the other methods in this class,
+ * however they will <strong>always</strong> be empty.
+ *
+ * @param keys The keys to delete. If <tt>nil</tt> or an empty array, <strong>all of this user's objects will be deleted.</strong>
+ * @param successHandler The block to be called when the objects have been populated.
+ * @param errorHandler The block to be called if the entire request failed (i.e. if there is no network connectivity).
+ */
 - (void)deleteValuesForKeys:(NSArray *)keys withUserCredentials:(CMUserCredentials *)credentials 
-             successHandler:(void (^)(NSDictionary *results, NSDictionary *errors))successHandler errorHandler:(void (^)(NSError *error))errorHandle;
+             successHandler:(void (^)(NSDictionary *results, NSDictionary *errors))successHandler errorHandler:(void (^)(NSError *error))errorHandler;
 
 @end
