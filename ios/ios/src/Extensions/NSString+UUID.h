7f32587a29700f60c71e44f6cf70cb3618d16fa3
Marc Weil <marc@cloudmine.me>
2011-11-25 18:20:43 -0500

    Add category to NSString for UUID generation.

---
+++ ios/ios/src/Extensions/NSString+UUID.h
@@ -0,0 +1,15 @@
+//
+//  NSString+UUID.h
+//  cloudmine-ios
+//
+//  Copyright (c) 2011 CloudMine, LLC. All rights reserved.
+//  See LICENSE file included with SDK for details.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (UUID)
+
++ (NSString *)stringWithUUID;
+
+@end
---
+++ ios/ios/src/Extensions/NSString+UUID.m
@@ -0,0 +1,20 @@
+//
+//  NSString+UUID.m
+//  cloudmine-ios
+//
+//  Copyright (c) 2011 CloudMine, LLC. All rights reserved.
+//  See LICENSE file included with SDK for details.
+//
+
+#import "NSString+UUID.h"
+
+@implementation NSString (UUID)
+
++ (NSString *)stringWithUUID {
+    CFUUIDRef uuid = CFUUIDCreate(kCFAllocatorDefault);
+    NSString *uuidString = (__bridge_transfer NSString *)CFUUIDCreateString(kCFAllocatorDefault, uuid);
+    CFRelease(uuid);
+    return uuidString;
+}
+
+@end
