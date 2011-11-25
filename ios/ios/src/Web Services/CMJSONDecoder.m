964aa0c4ee3a8150a02ed6f9ddd5a29fbafbb2af
Marc Weil <marc@cloudmine.me>
2011-11-25 16:43:37 -0500

    Add the JSON serialization and deseralization classes.

---
+++ ios/ios/src/Web Services/CMJSONDecoder.m
@@ -0,0 +1,13 @@
+//
+//  CMJSONDecoder.m
+//  cloudmine-ios
+//
+//  Copyright (c) 2011 CloudMine, LLC. All rights reserved.
+//  See LICENSE file included with SDK for details.
+//
+
+#import "CMJSONDecoder.h"
+
+@implementation CMJSONDecoder
+
+@end
---
+++ ios/ios/src/Web Services/CMJSONEncoder.m
@@ -0,0 +1,13 @@
+//
+//  CMJSONEncoder.m
+//  cloudmine-ios
+//
+//  Copyright (c) 2011 CloudMine, LLC. All rights reserved.
+//  See LICENSE file included with SDK for details.
+//
+
+#import "CMJSONEncoder.h"
+
+@implementation CMJSONEncoder
+
+@end
