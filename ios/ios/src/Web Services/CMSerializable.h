3ecd96cebf2a1577e1900df40e71d805b88fbeb2
Marc Weil <marc@cloudmine.me>
2011-11-26 16:09:07 -0500

    Docs for CMSerializable

--- ios/ios/src/Web Services/CMSerializable.h
+++ ios/ios/src/Web Services/CMSerializable.h
@@ -10,7 +10,20 @@
 
 @protocol CMSerializable <NSObject, NSCoding>
 
+/**
+ * Every object must have a form of unique identifier. Implement this
+ * in the classes that implement this protocol to provide that identifier.
+ */
 @property (atomic, readonly) NSString *objectId;
+
+/**
+ * The name of this class. This property must be overriden and implemented
+ * for cross-platform reasons. Choose a name that is consistent across all the
+ * platforms you are writing this app for (i.e. if all your Objective-C classes have
+ * a two-letter prefix and your Java classes for your Android version do not, you may
+ * want to stick with the non-prefixed class name for encoding and decoding purposes so
+ * everything matches up in each version of your app).
+ */
 @property (atomic, readonly) NSString *className;
 
 @end
