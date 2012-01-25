6a6ab6de8aa4918f0ec1f6702dfa800c0f4a2255
Marc Weil <marc@cloudmine.me>
2012-01-25 11:49:39 -0500

    Add test for default paging options

--- ios/iosTests/CMPagingDescriptorSpec.m
+++ ios/iosTests/CMPagingDescriptorSpec.m
@@ -17,13 +17,23 @@ describe(@"CMPagingDescriptor", ^{
     __block CMPagingDescriptor *descriptor = nil;
     
     context(@"given custom paging options", ^{
-        
         beforeEach(^{
             descriptor = [[CMPagingDescriptor alloc] initWithLimit:100 skip:10 includeCount:YES];
         });
         
         it(@"should render them as a query string properly", ^{
             NSString *expectedString = @"limit=100&skip=10&count=true";
+            [[[descriptor stringRepresentation] should] equal:expectedString];
+        });
+    });
+    
+    context(@"given default paging options", ^{
+        beforeEach(^{
+            descriptor = [CMPagingDescriptor defaultPagingDescriptor];
+        });
+        
+        it(@"should render them as a query string properly", ^{
+            NSString *expectedString = @"limit=50&skip=0&count=false";
             [[[descriptor stringRepresentation] should] equal:expectedString];
         });
     });
