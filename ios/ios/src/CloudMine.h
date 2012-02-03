bf16da0556975f22413052933305bab377aa70bf
Marc Weil <marc@cloudmine.me>
2012-02-03 13:26:29 -0500

    Add top-level include for all CloudMine headers.

---
+++ ios/ios/src/CloudMine.h
@@ -0,0 +1,25 @@
+//
+//  CMObjectClassNameRegistry.h
+//  cloudmine-ios
+//
+//  Copyright (c) 2011 CloudMine, LLC. All rights reserved.
+//  See LICENSE file included with SDK for details.
+//
+
+#import "CMAPICredentials.h"
+#import "CMDate.h"
+#import "CMFile.h"
+#import "CMGeoPoint.h"
+#import "CMObject.h"
+#import "CMObjectClassNameRegistry.m"
+#import "CMObjectDecoder.h"
+#import "CMObjectEncoder.h"
+#import "CMObjectSerialization.h"
+#import "CMPagingDescriptor.h"
+#import "CMSerializable.h"
+#import "CMServerFunction.h"
+#import "CMStore.h"
+#import "CMStoreCallbacks.h"
+#import "CMStoreOptions.h"
+#import "CMUser.h"
+#import "CMWebService.h"
