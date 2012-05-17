1db2b7c43d3af2061247ab5cbc6dd648f952fa2e
Derek Mansen <derek@cloudmine.me>
2012-05-17 11:48:43 -0400

    fix license on new files

--- ios/ios/src/Storage/Response/CMObjectFetchResponse.m
+++ ios/ios/src/Storage/Response/CMObjectFetchResponse.m
@@ -2,8 +2,8 @@
 //  CMObjectFetchResponse.m
 //  cloudmine-ios
 //
-//  Created by Derek Mansen on 5/9/12.
 //  Copyright (c) 2012 CloudMine, LLC. All rights reserved.
+//  See LICENSE file included with SDK for details.
 //
 
 #import "CMObjectFetchResponse.h"
--- ios/ios/src/Storage/Response/CMSnippetResult.h
+++ ios/ios/src/Storage/Response/CMSnippetResult.h
@@ -2,14 +2,14 @@
 //  CMSnippetResult.h
 //  cloudmine-ios
 //
-//  Created by Derek Mansen on 5/9/12.
 //  Copyright (c) 2012 CloudMine, LLC. All rights reserved.
+//  See LICENSE file included with SDK for details.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface CMSnippetResult : NSObject
 
-@property (strong, atomic) NSDictionary *result;
+@property (strong, nonatomic) id *result;
 
 @end
