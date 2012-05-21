42ce93066b6b68a89b5af506f83f769c6ce19c6a
Derek Mansen <derek@cloudmine.me>
2012-05-21 13:54:48 -0400

    update docs

--- ios/ios/src/CMUntypedObject.h
+++ ios/ios/src/CMUntypedObject.h
@@ -9,10 +9,11 @@
 #import "CMObject.h"
 
 /**
- * This is the default subclass of CMObject, used when the framework is unable to determine which
- * class an object should be deserialized into. This could happen if your application is creating
- * objects through the REST API or other means and not adding __class__ attributes. This class does
- * not attempt to deserialize object fields; it just leaves them as an accessible NSDictionary.
+ * This is a subclass of CMObject that has no type information. It is used when the framework is
+ * unable to determine which class an object should be deserialized into. This could happen if
+ * your application is creating objects through the REST API (or some other means) and not adding
+ * __class__ attributes. This class does not attempt to deserialize object fields; it just leaves
+ * them as an accessible NSDictionary.
  */
 @interface CMUntypedObject : CMObject
 
