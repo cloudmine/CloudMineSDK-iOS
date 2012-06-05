7019b0fe6ab222789a0b56ffed9a830674a36f04
Derek Mansen <derek@cloudmine.me>
2012-06-05 15:22:46 -0400

    fix comment

--- ios/ios/src/CMUntypedObject.h
+++ ios/ios/src/CMUntypedObject.h
@@ -12,7 +12,7 @@
  * This is a subclass of CMObject that has no type information. It is used when the framework is
  * unable to determine which class an object should be deserialized into. This could happen if
  * your application is creating objects through the REST API (or some other means) and not adding
- * __class__ attributes. This class does not attempt to deserialize object fields; it just leaves
+ * `__class__` attributes. This class does not attempt to deserialize object fields; it just leaves
  * them as an accessible NSDictionary.
  */
 @interface CMUntypedObject : CMObject
