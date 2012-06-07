3a91dda811b817d2ef2f10077f1c7a61bbf9ebe7
Marc Weil <marc@cloudmine.me>
2012-06-07 17:04:09 -0400

    Add a null store to represent an invalid store

---
+++ ios/ios/src/Storage/CMNullStore.h
@@ -0,0 +1,15 @@
+//
+//  CMNullStore.h
+//  cloudmine-ios
+//
+//  Copyright (c) 2012 CloudMine, LLC. All rights reserved.
+//  See LICENSE file included with SDK for details.
+//
+
+#import "CMStore.h"
+
+@interface CMNullStore : CMStore
+
++ (CMNullStore *)nullStore;
+
+@end
