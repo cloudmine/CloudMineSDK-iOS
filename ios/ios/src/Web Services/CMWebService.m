5ed756cf8d4ac7ffe49a2c01536861ff7b05093e
Conrad Kramer <conrad@kramerapps.com>
2012-06-26 13:13:00 -0400

    Added basic Reachability support

--- ios/ios/src/Web Services/CMWebService.m
+++ ios/ios/src/Web Services/CMWebService.m
@@ -6,6 +6,8 @@
 //  See LICENSE file included with SDK for details.
 //
 
+#import <SystemConfiguration/SystemConfiguration.h>
+
 #import <YAJLiOS/YAJL.h>
 
 #import "SPLowVerbosity.h"
@@ -27,13 +29,22 @@
 #define CM_SESSIONTOKEN_HEADER @"X-CloudMine-SessionToken"
 
 static __strong NSSet *_validHTTPVerbs = nil;
+static __strong NSString *CMReachabilityStatusChangedNotification = @"CMReachabilityStatusChangedNotification";
 typedef CMUserAccountResult (^_CMWebServiceAccountResponseCodeMapper)(NSUInteger httpResponseCode, NSError *error);
+
+typedef enum {
+    CMNotReachable,
+    CMReachableViaWiFi,
+    CMReachableViaWWAN
+} CMReachabilityStatus;
 
 NSString * const CMErrorDomain = @"CMErrorDomain";
 NSString * const NSURLErrorKey = @"NSURLErrorKey";
 NSString * const YAJLErrorKey = @"YAJLErrorKey";
 
-@interface CMWebService ()
+@interface CMWebService () {
+    SCNetworkReachabilityRef reachability;
+}
 @property (nonatomic, strong) NSString *apiUrl;
 - (NSURL *)constructTextUrlAtUserLevel:(BOOL)atUserLevel withKeys:(NSArray *)keys query:(NSString *)searchString pagingOptions:(CMPagingDescriptor *)paging sortingOptions:(CMSortDescriptor *)sorting withServerSideFunction:(CMServerFunction *)function extraParameters:(NSDictionary *)params;
 - (NSURL *)constructBinaryUrlAtUserLevel:(BOOL)atUserLevel withKey:(NSString *)key withServerSideFunction:(CMServerFunction *)function extraParameters:(NSDictionary *)params;
@@ -51,6 +62,10 @@ NSString * const YAJLErrorKey = @"YAJLErrorKey";
 @implementation CMWebService
 @synthesize networkQueue;
 @synthesize apiUrl;
+
+static void ReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkReachabilityFlags flags, void *info) {
+    [[NSNotificationCenter defaultCenter] postNotificationName:CMReachabilityStatusChangedNotification object:nil];
+}
 
 #pragma mark - Service initialization
 
@@ -72,11 +87,59 @@ NSString * const YAJLErrorKey = @"YAJLErrorKey";
     if ((self = [super init])) {
         self.networkQueue = [[NSOperationQueue alloc] init];
         self.apiUrl = CM_BASE_URL;
-
+        
+        reachability = SCNetworkReachabilityCreateWithName(NULL, [[[NSURL URLWithString:self.apiUrl] host] UTF8String]);
+        SCNetworkReachabilityContext context = {0, NULL, NULL, NULL, NULL};
+        SCNetworkReachabilitySetCallback(reachability, ReachabilityCallback, &context);
+        SCNetworkReachabilityScheduleWithRunLoop(reachability, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reachabilityStatusChanged:) name:CMReachabilityStatusChangedNotification object:nil];
+        
+        [self.networkQueue setSuspended:([self reachabilityStatus] == CMNotReachable)];
+        
         _appSecret = appSecret;
         _appIdentifier = appIdentifier;
     }
     return self;
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    CFRelease(reachability);
+}
+
+#pragma mark - Reachability
+
+- (void)reachabilityStatusChanged:(NSNotification *)notification {
+    CMReachabilityStatus currentStatus = [self reachabilityStatus];
+    
+    [self.networkQueue setSuspended:(currentStatus == CMNotReachable)];
+}
+
+- (CMReachabilityStatus)reachabilityStatus {
+    SCNetworkReachabilityFlags flags;
+	if (!SCNetworkReachabilityGetFlags(reachability, &flags))
+        return CMNotReachable;
+    
+    if ((flags & kSCNetworkReachabilityFlagsReachable) == 0)
+		return CMNotReachable;
+    
+    CMReachabilityStatus status = CMNotReachable;
+    
+	if ((flags & kSCNetworkReachabilityFlagsConnectionRequired) == 0) {
+		status = CMReachableViaWiFi;
+	}
+	
+	if ((((flags & kSCNetworkReachabilityFlagsConnectionOnDemand ) != 0) || (flags & kSCNetworkReachabilityFlagsConnectionOnTraffic) != 0)) {
+        if ((flags & kSCNetworkReachabilityFlagsInterventionRequired) == 0) {
+            status = CMReachableViaWiFi;
+        }
+    }
+	
+	if ((flags & kSCNetworkReachabilityFlagsIsWWAN) == kSCNetworkReachabilityFlagsIsWWAN) {
+        status = CMReachableViaWWAN;
+	}
+    
+    return status;
 }
 
 #pragma mark - GET requests for non-binary data
