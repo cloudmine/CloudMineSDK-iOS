9ccbef678b7ca47bac67b2ed309e50a306870581
Ethan Mick <ethan@cloudmine.me>
2012-12-14 12:53:05 -0500

    Updated CMAppDelegateBase to respond to push notifications.

--- ios/ios/src/Web Services/CMAppDelegateBase.h
+++ ios/ios/src/Web Services/CMAppDelegateBase.h
@@ -7,7 +7,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CMUser.h"
 
-@interface CMAppDelegateBase : UIResponder
+@class CMUser;
+
+@interface CMAppDelegateBase : UIResponder <UIApplicationDelegate>
+
+@property (atomic, strong) CMUser *user;
+@property (atomic, strong) CMUserResultCallback callback;
+
+
+/**
+ * If you implement this method in your Application Delegate (for example, to do something else with the devToken,
+ * then you MUST call super BEFORE doing anything to the devToken. We do not modify the devToken in anyway, so you
+ * should call super first to ensure we don't get a malformed token.
+ */
+- (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)devToken;
 
 @end
--- ios/ios/src/Web Services/CMAppDelegateBase.m
+++ ios/ios/src/Web Services/CMAppDelegateBase.m
@@ -10,4 +10,15 @@
 
 @implementation CMAppDelegateBase
 
+@synthesize user, callback;
+
+// Delegation methods
+- (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)devToken {
+    [self.user registerDeviceForPushNotificationsWithToken:devToken callback:self.callback];
+}
+
+- (void)application:(UIApplication *)app didFailToRegisterForRemoteNotificationsWithError:(NSError *)err {
+    NSLog(@"CloudMine *** Error in token registration with Apple. To Handle this error override application:didFailToRegisterForRemoteNotificationsWithError: in your own App Delegate. Error: %@", err);
+}
+
 @end
