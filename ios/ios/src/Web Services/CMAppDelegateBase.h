1d021ca44894fed4e7a69280ed644751eaa21685
Ethan Mick <ethan@cloudmine.me>
2012-12-14 12:11:01 -0500

    Adding in App Delegate file for handling push notifications.

---
+++ ios/ios/src/Web Services/CMAppDelegateBase.h
@@ -0,0 +1,13 @@
+//
+//  CMAppDelegateBase.h
+//  cloudmine-ios
+//
+//  Created by Ethan Mick on 12/14/12.
+//  Copyright (c) 2012 CloudMine, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMAppDelegateBase : UIResponder
+
+@end
---
+++ ios/ios/src/Web Services/CMAppDelegateBase.m
@@ -0,0 +1,13 @@
+//
+//  CMAppDelegateBase.m
+//  cloudmine-ios
+//
+//  Created by Ethan Mick on 12/14/12.
+//  Copyright (c) 2012 CloudMine, LLC. All rights reserved.
+//
+
+#import "CMAppDelegateBase.h"
+
+@implementation CMAppDelegateBase
+
+@end
