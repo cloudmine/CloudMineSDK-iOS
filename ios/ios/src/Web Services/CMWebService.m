e790224c900e544b60d3a010029f1b6217005ea4
Ethan Mick <ethan@cloudmine.me>
2013-02-27 10:19:25 -0500

    Added backgrounding to all requests.

--- ios/ios/src/Web Services/CMWebService.m
+++ ios/ios/src/Web Services/CMWebService.m
@@ -680,6 +680,10 @@ NSString * const YAJLErrorKey = @"YAJLErrorKey";
                 }
             }];
             
+            [requestOperation setShouldExecuteAsBackgroundTaskWithExpirationHandler:^{
+                
+            }];
+            
             [self enqueueHTTPRequestOperation:requestOperation];
         };
 
@@ -1485,6 +1489,11 @@ NSString * const YAJLErrorKey = @"YAJLErrorKey";
     }];
     
     [self enqueueHTTPRequestOperation:requestOperation];
+}
+
+- (void)enqueueHTTPRequestOperation:(AFHTTPRequestOperation *)operation {
+    [operation setShouldExecuteAsBackgroundTaskWithExpirationHandler:nil];
+    [super enqueueHTTPRequestOperation:operation];
 }
 
 - (void)performBlock:(void (^)())block {
