213cf65cbcaeabc103ec7e06793433ab1bb63980
Ethan Mick <ethan@ethanmick.com>
2013-10-24 10:56:59 -0400

    Use the system object serialization.

--- ios/ios/src/Web Services/Encoding/CMJSONEncoder.m
+++ ios/ios/src/Web Services/Encoding/CMJSONEncoder.m
@@ -6,21 +6,21 @@
 //  See LICENSE file included with SDK for details.
 //
 
-#import <YAJLiOS/YAJL.h>
 #import "CMJSONEncoder.h"
+#import "NSDictionary+CMJSON.h"
 
 @implementation CMJSONEncoder
 
 #pragma mark - Kickoff methods
 
 + (NSString *)encodeObjects:(id<NSFastEnumeration>)objects {
-    return [[super encodeObjects:objects] yajl_JSONString];
+    return [[super encodeObjects:objects] jsonString];
 }
 
 #pragma mark - Translation methods
 
 - (NSString *)encodedRepresentation {
-    return [_encodedData yajl_JSONString];
+    return [_encodedData jsonString];
 }
 
 @end
