30dc91bcca326cd4a8a06bb8baccee0806772aa9
Ethan Mick <ethanmski@gmail.com>
2014-10-07 12:40:39 -0400

    Removed unused string.

--- ios/iosTests/CMExtensionsSpec.m
+++ ios/iosTests/CMExtensionsSpec.m
@@ -78,10 +78,8 @@ describe(@"CMExtensions", ^{
             NSDictionary *query = @{@"value1": @"1", @"value2" : @"string"};
             NSURL *url = [NSURL URLWithString:CM_BASE_URL];
             url = [url URLByAppendingAndEncodingQueryParameters:query];
-            NSString *final = [NSString stringWithFormat:@"%@?value1=1&value2=string", CM_BASE_URL];
+            
             [[url.absoluteString should] containString:@"https://api.cloudmine.me/?"];
-            
-            
             [[url.absoluteString should] containString:@"value1=1"];
             [[url.absoluteString should] containString:@"value2=string"];
         });
