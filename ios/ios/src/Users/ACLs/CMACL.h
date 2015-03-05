8c011deeb2dabe507c02694c531d6cb4b8259955
Ethan Mick <ethan@ethanmick.com>
2015-03-05 15:08:01 -0500

    Added "segments" Dictionary to the ACL class. Added in the additional strings to use segments. Serialize it correctly.

--- ios/ios/src/Users/ACLs/CMACL.h
+++ ios/ios/src/Users/ACLs/CMACL.h
@@ -19,6 +19,13 @@ extern NSString * const CMACLUpdatePermission;
 extern NSString * const CMACLDeletePermission;
 
 /**
+ * These are segments that can be used to share the objects with a large group of
+ * users easily.
+ */
+extern NSString * const CMACLSegmentPublic;
+extern NSString * const CMACLSegmentLoggedIn;
+
+/**
  * This is a class to represent an ACL object in CloudMine's data store. CMACLs can
  * be added to CMObjects to share CMObjects with other users. The users specified in
  * the members property are granted the permissions specified in the permissions property.
@@ -30,6 +37,12 @@ extern NSString * const CMACLDeletePermission;
  * The set of IDs of the members which are granted the permissions provided in the ACL.
  */
 @property (nonatomic, strong) NSSet *members;
+
+/**
+ * Additional ACL segments that can be used. These will override the members arrary and can be used to 
+ * share the objects with a larger scope.
+ */
+@property (nonatomic, strong) NSMutableDictionary *segments;
 
 /**
  * The set of permissions that the members of the ACL are granted. Can be any combination of
