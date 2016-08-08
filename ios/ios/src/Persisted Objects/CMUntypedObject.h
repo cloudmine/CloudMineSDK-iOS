444f6586dc9f8f39c6c3f351908cce3757fff026
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-08-08 10:08:08 -0400

    Adds nullability annotations for CMUntypedObject class

--- ios/ios/src/Persisted Objects/CMUntypedObject.h
+++ ios/ios/src/Persisted Objects/CMUntypedObject.h
@@ -8,6 +8,8 @@
 
 #import "CMObject.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
 /**
  * This is a subclass of CMObject that has no type information. It is used when the framework is
  * unable to determine which class an object should be deserialized into. This could happen if
@@ -25,11 +27,15 @@
 /**
  Creates a CMObject that could not be mapped to a class.
  */
-- (instancetype)initWithFields:(NSDictionary *)theFields objectId:(NSString *)objId;
+- (instancetype)initWithFields:(nullable NSDictionary *)theFields objectId:(NSString *)objId;
 
 /**
  For the super class
  */
 - (instancetype)initWithObjectId:(NSString *)theObjectId NS_DESIGNATED_INITIALIZER;
 
+- (instancetype)init NS_UNAVAILABLE;
+
 @end
+
+NS_ASSUME_NONNULL_END
