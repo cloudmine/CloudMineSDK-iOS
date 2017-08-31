7c331eab4ea87aa47eccf4a6e45ddb71c43164f1
Ben DiFrancesco <ben@scopelift.co>
2017-08-31 11:01:51 -0400

    Remove superfluous colons from NSURL category header docs

--- ios/ios/src/Extensions/NSURL+QueryParameterAdditions.h
+++ ios/ios/src/Extensions/NSURL+QueryParameterAdditions.h
@@ -12,15 +12,15 @@
 
 /**
  * Appends a key value to a query string url encoding the value
- * @param key: The key of the query string item
- * @param value: The value of the query string item
+ * @param key The key of the query string item
+ * @param value The value of the query string item
  * @returns A new URL
  */
 -(NSURL *)URLByAppendingAndEncodingQueryParameter:(NSString *)key andValue:(NSString *)value;
 
 /**
  * Appends a collection of key value pairs to a query string url encoding the values
- * @param queryParameters: the key values to add
+ * @param queryParameters the key values to add
  * @return A new URL
  */
 -(NSURL *)URLByAppendingAndEncodingQueryParameters:(NSDictionary *)queryParameters;
